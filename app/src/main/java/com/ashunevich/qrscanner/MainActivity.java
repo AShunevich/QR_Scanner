7ce0fefd991d4f86b0986c0fbc0db5cd1910f132
AShunevich <48925983+AShunevich@users.noreply.github.com>
2021-06-07 16:54:02 +0300

    - code update - library update

--- app/src/main/java/com/ashunevich/qrscanner/MainActivity.java
+++ app/src/main/java/com/ashunevich/qrscanner/MainActivity.java
@@ -13,9 +13,17 @@ import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.util.Size;
+import android.webkit.URLUtil;
 import android.widget.Toast;
 
 import com.ashunevich.qrscanner.databinding.ActivityMainBinding;
@@ -36,9 +44,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate (getLayoutInflater ());
         setContentView(binding.getRoot());
-
+        setClickTener();
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         requestCamera();
+        setTextChangeListenere();
     }
 
     private void requestCamera() {
@@ -55,11 +64,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult (requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_REQUEST_CAMERA) {
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startCamera();
+                startCamera ();
             } else {
-                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
+                Toast.makeText (this, "Camera Permission Denied", Toast.LENGTH_SHORT).show ();
             }
         }
     }
@@ -78,6 +88,35 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void setClickTener(){
+
+        binding.textView2.setOnClickListener (view -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse(binding.textView2.getText ().toString ()));
+            startActivity(browserIntent);
+        });
+
+    }
+
+    private void setTextChangeListenere(){
+        binding.textView2.addTextChangedListener (new TextWatcher () {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                checkHyperlink(editable.toString ());
+            }
+        });
     }
 
     private void bindPreview(ProcessCameraProvider cameraProvider) {
@@ -101,7 +140,11 @@ public class MainActivity extends AppCompatActivity {
         imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new QRCodeImageAnalyzer(new QRCodeFoundListener() {
             @Override
             public void onQRCodeFound(String _qrCode) {
-               binding.textView2.setText (_qrCode);
+                if(!_qrCode.equals (binding.textView2.getText ().toString ())){
+                    binding.textView2.setText("");
+                    binding.textView2.setText (_qrCode);
+                }
+
             }
 
             @Override
@@ -110,5 +153,21 @@ public class MainActivity extends AppCompatActivity {
             }
         }));
         Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, imageAnalysis, preview);
+    }
+
+    private void checkHyperlink(String s){
+        boolean isValid = URLUtil.isValidUrl(s) && Patterns.WEB_URL.matcher(s).matches();
+        if(isValid){
+           binding.textView2.setEnabled (true);
+            binding.textView2.setClickable (true);
+            binding.textView2.setTextColor (Color.BLUE);
+           binding.textView2.setTypeface (null, Typeface.ITALIC);
+        }
+        else{
+            binding.textView2.setEnabled (false);
+            binding.textView2.setClickable (false);
+            binding.textView2.setTextColor (binding.textView.getCurrentTextColor ());
+            binding.textView2.setTypeface (null, Typeface.NORMAL);
+        }
     }
     }
